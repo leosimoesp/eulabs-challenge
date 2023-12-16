fe373e57ec814af37d60af86979551e32ce308ba
Leonardo Simões <leolbsti@gmail.com>
2023-12-16 18:59:31 -0300

    feat: implement usecase get product by code

---
+++ core/usecase/product_get.go
@@ -0,0 +1,50 @@
+package usecase
+
+import (
+	"context"
+	"time"
+
+	"github.com/lbsti/eulabs-challenge/core/repository"
+)
+
+type ProductGet struct {
+	repository repository.ProductRepository
+}
+
+type ProductGetOutputDTO struct {
+	CreatedAt    string `json:"createdAt"`
+	UpdatedAt    string `json:"updatedAt"`
+	Title        string `json:"title"`
+	Description  string `json:"description"`
+	Code         string `json:"code"`
+	Reference    string `json:"reference"`
+	PriceInCents int64  `json:"priceInCents"`
+	ID           int64  `json:"id"`
+}
+
+func NewProductGet(productRepo repository.ProductRepository) *ProductGet {
+	return &ProductGet{
+		repository: productRepo,
+	}
+}
+
+func (p *ProductGet) Execute(ctx context.Context, code string) (ProductGetOutputDTO, error) {
+
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, time.Duration(ProductCreateTimeout))
+	defer cancel()
+	productData, err := p.repository.GetByCode(ctxWithTimeout, code)
+
+	if err != nil {
+		return ProductGetOutputDTO{}, err
+	}
+	return ProductGetOutputDTO{
+		ID:           productData.ID,
+		Title:        productData.Title,
+		CreatedAt:    productData.CreatedAt,
+		UpdatedAt:    productData.UpdatedAt,
+		Reference:    productData.Reference,
+		Code:         productData.Code,
+		PriceInCents: productData.PriceInCents,
+		Description:  productData.Description,
+	}, nil
+}
